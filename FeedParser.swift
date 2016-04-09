c5b4480c21422897b51bc45632fffe2c1de236cf
OctoberHammer <octoberhammer@gmail.com>
2016-04-09 19:15:40 +0300

    no message

--- FeedParser.swift
+++ FeedParser.swift
@@ -9,11 +9,23 @@
 import UIKit
 
 class FeedParser: NSObject, NSXMLParserDelegate {
-	private var rssItems:[(title: String, link: String, description: String, pubDate: String, guid: String, duration: String)] = []
+	private var rssItems:[(title: String, link: String, description: String, pubDate: String, guid: String, duration: String, itunesAuthor: String, itunesSubtitle: String)] = []
 	private var currentElement: String = ""
 	private var currentTitle: String = "" {
 		didSet {
 			currentTitle = currentTitle.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+		}
+	}
+	
+	private var currentItunesAuthor: String = "" {
+		didSet {
+			currentItunesAuthor = currentItunesAuthor.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+		}
+	}
+	
+	private var currentItunesSubtitle: String = "" {
+		didSet {
+			currentItunesSubtitle = currentItunesSubtitle.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
 		}
 	}
 	
@@ -47,6 +59,98 @@ class FeedParser: NSObject, NSXMLParserDelegate {
 		}
 	}
 	
-	private var parserCompletionHandler: ([(title: String, link: String, description: String, pubDate: String, guid: String, duration: String)] -> Void)?
+	private var parserCompletionHandler: ([(title: String, link: String, description: String, pubDate: String, guid: String, duration: String, itunesAuthor: String, itunesSubtitle: String)] -> Void)?
+	
+	func parseFeed(feedUrl: String, completionHandler: ([(title: String, link: String, description: String, pubDate: String, guid: String, duration: String, itunesAuthor: String, itunesSubtitle: String)] -> Void)?) -> Void {
+		self.parserCompletionHandler = completionHandler
+		
+		let request = NSURLRequest(URL: NSURL(string:feedUrl)!)
+		let urlSession = NSURLSession.sharedSession()
+		let task = urlSession.dataTaskWithRequest(request, completionHandler: {
+			(data, response, error) -> Void in
+			
+			guard let data = data else {
+				if let error = error {
+					print(error)
+				}
+				return
+			}
+			
+			let parser = NSXMLParser(data:data)
+			parser.delegate = self
+			parser.parse()
+		})
+		
+		task.resume()
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	func parserDidStartDocument(parser: NSXMLParser) {
+		rssItems = []
+	}
+	
+	
+	
+	func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+		currentElement = elementName
+		
+		if currentElement == "item" {
+			currentTitle = ""
+			currentLink = ""
+			currentGuid = ""
+			currentPubDate = ""
+			currentDuration = ""
+			currentDescription = ""
+			currentItunesAuthor = ""
+			currentItunesSubtitle = ""
+		}
+	}
+	
+
+	func parser(parser: NSXMLParser, foundCharacters string: String) {
+		switch currentElement {
+		case "title": currentTitle += string
+		case "description": currentDescription += string
+		case "pubdate": currentPubDate += string
+		case  "duration": currentDuration += string
+		case "link": currentLink += string
+		case "guid": currentGuid += string
+		case "itunes:author": currentItunesAuthor += string
+		case "itunes:subtitle": currentItunesSubtitle += string
+		default: break
+		}
+	}
+	
+	func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+		if elementName == "item" {
+			let rssItem = (title: currentTitle, link: currentLink, description: currentDescription, pubDate: currentPubDate, guid: currentGuid, duration: currentDuration, itunesAuthor: currentItunesAuthor, itunesSubtitle: currentItunesSubtitle)
+			rssItems += [rssItem]
+		}
+	}
+	
+	func parserDidEndDocument(parser: NSXMLParser) {
+		parserCompletionHandler?(rssItems)
+	}
+	
+	
+	func parser(parser: NSXMLParser, parseErrorOccurred parseError: NSError) {
+		print(parseError.localizedDescription)
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
--- SimpleRSSReader/NewsTableViewController.swift
+++ SimpleRSSReader/NewsTableViewController.swift
@@ -9,14 +9,27 @@
 import UIKit
 
 class NewsTableViewController: UITableViewController {
-    
+	
+	private var rssItems: [(title: String, link: String, description: String, pubDate: String, guid: String, duration: String, itunesAuthor: String, itunesSubtitle: String)]?
+	
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.estimatedRowHeight = 155.0
         tableView.rowHeight = UITableViewAutomaticDimension
-
-    }
+		
+		let feedParser = FeedParser()
+		feedParser.parseFeed("https://feeds.feedburner.com/EnglishAsASecondLanguagePodcast",
+			completionHandler: {
+				(rssItems: [(title: String, link: String, description: String, pubDate: String, guid: String, duration: String, itunesAuthor: String, itunesSubtitle: String)]) -> Void
+			in
+				self.rssItems = rssItems
+				NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+					self.tableView.reloadSections(NSIndexSet(index: 0), withRowAnimation: .None)
+			})
+    })
+		
+	}
 
 
     override func didReceiveMemoryWarning() {
@@ -34,8 +47,30 @@ class NewsTableViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Return the number of rows in the section.
-        return 0
+		guard let rssItems = rssItems else {
+			return 0
+		}
+		return rssItems.count
     }
 
-
+	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! NewsTableViewCell
+		if let item = rssItems?[indexPath.row] {
+			cell.titleLabel.text = item.title
+			cell.descriptionLabel.text = item.itunesSubtitle
+			cell.dateLabel.text = item.pubDate
+			cell.tag = indexPath.row
+		}
+		return cell
+	}
+	
+	
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		if segue.identifier == "showSpecificEpisode" {
+			let sendingCell = sender as! NewsTableViewCell;
+			//print("\(sendingCell.tag)")
+			let index = sendingCell.tag;
+			let currentEpisode = rssItems?[index]
+		}
+	}
 }
